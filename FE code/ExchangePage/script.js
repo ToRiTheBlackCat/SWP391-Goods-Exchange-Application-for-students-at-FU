9b22624e88d6263c0bcd2337402e55e92f926bf5
Ta Duc Thang <thangtdse184727@fpt.edu.vn>
2024-05-17 15:31:45 +0700

    Chỉnh sửa exchange page

--- FE code/ExchangePage/script.js
+++ FE code/ExchangePage/script.js
@@ -1,8 +1,7 @@
 document.getElementById('confirmExchange').addEventListener('click', function() {
-    alert('Bạn đã xác nhận trao đổi.');
-  });
-  
-  document.getElementById('declineExchange').addEventListener('click', function() {
-    alert('Bạn đã từ chối trao đổi.');
-  });
-  
+  alert('Bạn đã xác nhận trao đổi.');
+});
+
+document.getElementById('declineExchange').addEventListener('click', function() {
+  alert('Bạn đã từ chối trao đổi.');
+});
