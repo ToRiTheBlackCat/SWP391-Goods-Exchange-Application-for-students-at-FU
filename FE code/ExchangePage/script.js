712bbdd49d827644f6f5d241185b7ac5ffc0dc9f
Ta Duc Thang <thangtdse184727@fpt.edu.vn>
2024-05-17 14:08:16 +0700

    tạo chooseProduct và exchangePage

---
+++ FE code/ExchangePage/script.js
@@ -0,0 +1,8 @@
+document.getElementById('confirmExchange').addEventListener('click', function() {
+    alert('Bạn đã xác nhận trao đổi.');
+  });
+  
+  document.getElementById('declineExchange').addEventListener('click', function() {
+    alert('Bạn đã từ chối trao đổi.');
+  });
+  
