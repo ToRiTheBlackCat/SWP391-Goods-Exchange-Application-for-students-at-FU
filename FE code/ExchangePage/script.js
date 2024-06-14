43c048b0b6f36063ef491899b1030150ef7580fb
Ta Duc Thang <thangtdse184727@fpt.edu.vn>
2024-06-14 15:49:34 +0700

    update

--- FE code/ExchangePage/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('confirmExchange').addEventListener('click', function() {
-  alert('Bạn đã xác nhận trao đổi.');
-});
-
-document.getElementById('declineExchange').addEventListener('click', function() {
-  alert('Bạn đã từ chối trao đổi.');
-});
