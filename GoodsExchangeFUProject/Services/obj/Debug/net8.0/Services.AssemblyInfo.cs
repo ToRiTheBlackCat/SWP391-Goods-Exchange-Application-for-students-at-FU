aa666fb9db97a42e524c767eb05232f286661539
Nguyen Huynh Minh Tri <triminh0502@gmail.com>
2024-06-15 15:32:43 +0700

    Update: Add AverageScore of ProductOwner

--- GoodsExchangeFUProject/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ GoodsExchangeFUProject/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc87021876a15423de11294d3049cc8f5d4bf88d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9079784916bdb8f3879909be554ff908f1411728")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
